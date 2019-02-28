3eaa996247b4b672512c8f35e2bdb3a8c19c6b5c
Rob Parker <rjp23@le.ac.uk>
2019-02-28 21:26:37 +0000

    updated

--- bokeh-app/main.py
+++ bokeh-app/main.py
@@ -5,6 +5,8 @@ from holoviews import opts
 hv.extension('bokeh', 'matplotlib', width="100")
 import hvplot.xarray
 
+renderer = hv.renderer('bokeh')
+
 ds = xarray.open_dataset('data/ds_transcom_merged.nc')
 
 #change names back from bytestrings to strings (python2->3 thing?)
@@ -15,4 +17,4 @@ ds = ds.rename({'model_id':'Model Name', 'transcom':'Region'})
 
 scat = ds.hvplot.scatter(x='tas_change', y='tsl_change', groupby='Region', height=900, width=900)
 
-scat.servable()
+doc = renderer.server_doc(scat)doc.title = 'HoloViews App'
