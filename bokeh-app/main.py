3418ddb1985e751810bf85a16cf82d2298f6a104
Rob Parker <rjp23@le.ac.uk>
2019-02-28 22:16:29 +0000

    updated

--- bokeh-app/main.py
+++ bokeh-app/main.py
@@ -16,5 +16,5 @@ ds = ds.rename({'model_id':'Model Name', 'transcom':'Region'})
 
 scat = ds.hvplot.scatter(x='tas_change', y='tsl_change', groupby='Region', height=900, width=900)
 
-doc = renderer.server_doc(scat)
-doc.title = 'HoloViews App'
+scat
+
